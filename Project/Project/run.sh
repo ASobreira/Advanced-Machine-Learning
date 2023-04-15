9a31a71aba2e09f719b15205d334a12154db96d0
asobreira93 <110920663+MrVague@users.noreply.github.com>
2023-04-15 17:12:45 +0100

    added relevant commands for server and changed run.sh

--- Project/Project/run.sh
+++ Project/Project/run.sh
@@ -1,5 +1,2 @@
-#inclui imports necessarios e comandos para 
-#correr codigo no servidor
-
-RUN pip install -r requirements.txt
-python teste2.py > results.txt
+pip install -r requirements.txt
+python teste.py > results.txt
