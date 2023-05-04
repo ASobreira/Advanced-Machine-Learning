b630f4495303568dd33ee05faef89eccbc84db69
asobreira93 <110920663+MrVague@users.noreply.github.com>
2023-05-04 10:52:52 +0100

    server test

---
+++ Project/Project/svm_server_script.py
@@ -0,0 +1,590 @@
+### packages
+import numpy as np
+import pandas as pd 
+import os
+import cv2
+from sklearn.svm import SVC
+from skimage.feature import hog
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from skimage.color import rgb2gray
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D, Flatten, Dense, Dropout, GlobalAveragePooling2D
+from keras.applications import VGG16
+from skimage.feature import hog
+from skimage import data, exposure
+import random
+from keras import layers, losses
+from keras.models import Model
+from sklearn.metrics import classification_report, accuracy_score, matthews_corrcoef, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+import seaborn as sns
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+import math
+from keras.utils.np_utils import to_categorical
+from sklearn.metrics import classification_report, confusion_matrix
+from datetime import datetime
+
+initial_time = datetime.now()
+initial_time = initial_time.strftime("%H:%M:%S")
+
+### Functions
+
+def display(img):
+    plt.imshow(img)
+    plt.show()
+
+def prediction_metrics(model, X_test, y_test, classes):
+    preds = []
+    truth = []
+    y_hat = model.predict(X_test)
+
+    for i in range(X_test.shape[0]):
+        
+        predict_index = np.argmax(y_hat[i])
+        preds.append(predict_index) 
+        
+        true_index = np.argmax(y_test[i])
+        truth.append(true_index)
+        
+    mcc = matthews_corrcoef(truth, preds)
+    print("MCC: ", mcc)
+    label = list(set(classes))
+    cm = confusion_matrix(truth, preds)
+    sns.heatmap(cm, annot=True, cmap="Blues", fmt="d", xticklabels=label, yticklabels=label)
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.title('Confusion Matrix')
+    plt.show()
+    print(classification_report(truth, preds))
+
+### Data
+print("data gathering started")
+#path martim
+path = "../../../../DadosProj/UTKFace/"
+#path alex
+#path = "../../../data_project/UTKFace/"
+
+files = os.listdir(path)
+size = len(files)
+print("Total samples:",size)
+print(files[0])
+
+#### Data Pre processing
+images = []
+ages = []
+genders = []
+ethnicities = []
+counter = 0
+
+for file in files:
+    if counter == 1000:
+        break
+    
+    image = cv2.imread(path+file)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, dsize=(200, 200))
+    image = image.reshape((image.shape[0], image.shape[1], 3)) ### A ausencia deste ponto  estava a causar o erro no ORB
+    #image = image / 255.0 #normalizing values on each point 
+    
+    images.append(image)
+    delimit = file.split('_')
+    ages.append(int(delimit[0]))
+    # first number is age (0-116 possible values)
+    genders.append(int(delimit[1]))
+    # second number is gender (0 for male, 1 for female)
+    #ethnicities.append(int(delimit[2]))
+    # third number is ethnicity (0 for white, 1 for black, 2 for asian, 3 for indian, 4 for any other ethnicity)
+
+    counter += 1
+
+print("data gathering finished")
+
+### Age classes
+print("Age Classes started")
+age_classes = []
+for i in ages:
+    i = int(i)
+    if i <= 1:
+        age_classes.append(0)
+    if (i>1) and (i<=13):
+        age_classes.append(1)
+    if (i>13) and (i<17):
+        age_classes.append(2)
+    if (i>=17) and (i<30):
+        age_classes.append(3)
+    if (i>=30) and (i<60):
+        age_classes.append(4)
+    if i>=60:
+        age_classes.append(5)
+
+age_labels = ["Baby",   # index 0
+        "Infant",       # index 1
+        "Adolescent",   # index 2 
+        "Young Adult",  # index 3 
+        "Adult"         # index 4
+        "Old Adult",    # index 5
+        ]
+
+age_categories = to_categorical(age_classes, num_classes=6)
+age_categories[:20]
+print("Age Classes finished")
+
+### Plots Data distribution
+
+x_genders = list(set(genders))
+y_genders= [genders.count(i) for i in x_genders]
+plt.bar(x_genders,y_genders)
+plt.show()
+print("Max value:",max(genders))
+plt.savefig('Plots Data distribution Gender.png')
+
+x_ages = list(set(age_classes))
+y_ages = [age_classes.count(i) for i in x_ages]
+plt.bar(x_ages,y_ages)
+plt.show()
+print("Max value:",max(age_classes))
+plt.savefig('Plots Data distribution Age.png')
+
+#### SVM
+
+##### BASE
+print("BASE SVM started")
+flattened_images = []
+
+for image in images:
+    flattened_image = image.flatten()
+    flattened_images.append(flattened_image)
+
+flattened_images_array = np.array(flattened_images)
+
+print(flattened_images_array.shape)
+
+###### Gender
+X_train, X_test, y_train, y_test = train_test_split(flattened_images_array, genders, test_size=0.2, random_state=42)
+
+clf = SVC()
+clf.fit(X_train, y_train)
+
+# Evaluate the classifier on the testing data
+y_pred = clf.predict(X_test)
+
+mcc = matthews_corrcoef(y_test, y_pred)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+#print("Accuracy: ", accuracy)
+print(classification_report(y_test, y_pred))
+#confusion_matrix(y_test, y_pred)
+
+labelGender = list(set(genders))
+
+# assume y_true and y_pred are the true and predicted labels, respectively
+cm = confusion_matrix(y_test, y_pred)
+
+# create a heatmap of the confusion matrix using seaborn
+sns.heatmap(cm, annot=True, cmap="Blues", fmt="d", xticklabels=labelGender, yticklabels=labelGender)
+
+# set plot labels and title
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+plt.savefig('CM SVM Gender.png')
+
+##### Age
+X_train, X_test, y_train, y_test = train_test_split(flattened_images_array, age_categories, test_size=0.2, random_state=42)
+
+clf = OneVsRestClassifier(SVC())
+clf.fit(X_train, y_train)
+
+# Convert one-hot encoded labels to integer form
+y_pred = clf.predict(X_test)
+
+y_test_int = np.argmax(y_test, axis=1)
+y_pred_int = np.argmax(y_pred, axis=1)
+
+# Print classification report and confusion matrix
+
+mcc = matthews_corrcoef(y_test_int, y_pred_int)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+print(classification_report(y_test_int, y_pred_int))
+print(confusion_matrix(y_test_int, y_pred_int))
+print("BASE SVM finished")
+
+#### HOG
+
+print("HOG started")
+# Extract HOG features from the images
+hog_features = []
+for img in images:
+    #hog_feature = hog(img, orientations=9, pixels_per_cell=(8, 8),
+    #                cells_per_block=(2, 2), transform_sqrt=True, feature_vector=True, multichannel=True, channel_axis=2)
+    hog_feature = hog(img, orientations=9, pixels_per_cell=(8, 8),
+                    cells_per_block=(2, 2), transform_sqrt=True, feature_vector=True, channel_axis=2)
+    
+    hog_features.append(hog_feature)
+hog_features = np.array(hog_features)
+
+# Normalize the features to have zero mean and unit variance
+mean = np.mean(hog_features, axis=0)
+std = np.std(hog_features, axis=0)
+hog_features_norm = (hog_features - mean) / std
+
+##### Gender
+X_train, X_test, y_train, y_test = train_test_split(hog_features_norm, genders, test_size=0.2, random_state=42)
+
+clf = SVC()
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+# Print classification report and confusion matrix
+
+mcc = matthews_corrcoef(y_test, y_pred)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+print(classification_report(y_test, y_pred))
+print(confusion_matrix(y_test, y_pred))
+
+#### AGE
+X_train, X_test, y_train, y_test = train_test_split(hog_features_norm, age_categories, test_size=0.2, random_state=42)
+clf = OneVsRestClassifier(SVC())
+clf.fit(X_train, y_train)
+
+# Convert one-hot encoded labels to integer form
+y_pred = clf.predict(X_test)
+
+y_test_int = np.argmax(y_test, axis=1)
+y_pred_int = np.argmax(y_pred, axis=1)
+
+# Print classification report and confusion matrix
+
+mcc = matthews_corrcoef(y_test_int, y_pred_int)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+print(classification_report(y_test_int, y_pred_int))
+print(confusion_matrix(y_test_int, y_pred_int))
+print("HOG finished")
+
+
+#### ORB
+print("ORB Started")
+orb = cv2.ORB_create()
+# define minimum and maximum keypoints
+MIN_KEYPOINTS = 100
+orb_features = []
+orb_genders = []
+orb_ages = []
+
+# loop over images and keypoints
+for i, img in enumerate(images):
+    keypoints, descriptors = orb.detectAndCompute(img, None)
+    keypoints, descriptors = orb.compute(image_check, keypoints)
+    keypoints = list(keypoints)    
+    # check if number of keypoints is between min and max
+    if len(keypoints) < MIN_KEYPOINTS:
+        continue
+        
+    # remove extra keypoints randomly
+    while len(keypoints) > MIN_KEYPOINTS:
+        idx = random.randint(0, len(keypoints)-1)
+        keypoints.pop(idx)
+        descriptors = np.delete(descriptors, idx, axis=0)
+        
+    # add descriptors to feature list
+    orb_features.append(np.ndarray.flatten(descriptors))
+    orb_genders.append(genders[i])
+    orb_ages.append(age_categories[i])
+
+mean = np.mean(orb_features, axis=0)
+std = np.std(orb_features, axis=0)
+orb_features_norm = (orb_features - mean) / std
+
+##### GEnder
+X_train, X_test, y_train, y_test = train_test_split(orb_features_norm, orb_genders, test_size=0.2, random_state=42)
+
+clf = SVC()
+clf.fit(X_train, y_train)
+
+# Evaluate the classifier on the testing data
+y_pred = clf.predict(X_test)
+
+mcc = matthews_corrcoef(y_test, y_pred)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+#print("Accuracy: ", accuracy)
+print(classification_report(y_test, y_pred))
+#confusion_matrix(y_test, y_pred)
+
+labelGender = list(set(genders))
+
+# assume y_true and y_pred are the true and predicted labels, respectively
+cm = confusion_matrix(y_test, y_pred)
+
+# create a heatmap of the confusion matrix using seaborn
+sns.heatmap(cm, annot=True, cmap="Blues", fmt="d", xticklabels=labelGender, yticklabels=labelGender)
+
+# set plot labels and title
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+plt.savefig('CM SVM Gender ORB.png')
+
+
+##### AGE
+X_train, X_test, y_train, y_test = train_test_split(orb_features_norm, orb_ages, test_size=0.2, random_state=42)
+
+clf = OneVsRestClassifier(SVC())
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+y_test_int = np.argmax(y_test, axis=1)
+y_pred_int = np.argmax(y_pred, axis=1)
+
+# Print classification report and confusion matrix
+
+mcc = matthews_corrcoef(y_test_int, y_pred_int)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+print(classification_report(y_test_int, y_pred_int))
+print(confusion_matrix(y_test_int, y_pred_int))
+print("ORB Finished")
+
+#### VGG16
+VGG_model = VGG16(weights='imagenet', include_top=False, input_shape=(200, 200, 3))
+
+for layer in VGG_model.layers:
+	layer.trainable = False
+	
+VGG_model.summary()
+
+X = np.stack(images)
+X = VGG_model.predict(X)
+
+##### Gender
+X_svm = X.reshape(X.shape[0], -1)
+X_train, X_test, y_train, y_test = train_test_split(X_svm, genders, test_size=0.2, random_state=42)
+
+clf = SVC()
+clf.fit(X_train, y_train)
+
+# Evaluate the classifier on the testing data
+y_pred = clf.predict(X_test)
+
+mcc = matthews_corrcoef(y_test, y_pred)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+#print("Accuracy: ", accuracy)
+print(classification_report(y_test, y_pred))
+#confusion_matrix(y_test, y_pred)
+
+labelGender = list(set(genders))
+
+# assume y_true and y_pred are the true and predicted labels, respectively
+cm = confusion_matrix(y_test, y_pred)
+
+# create a heatmap of the confusion matrix using seaborn
+sns.heatmap(cm, annot=True, cmap="Blues", fmt="d", xticklabels=labelGender, yticklabels=labelGender)
+
+# set plot labels and title
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+plt.savefig('CM VGG16 SVM Gender.png')
+
+#### AGE
+X_train, X_test, y_train, y_test = train_test_split(X_svm, age_categories, test_size=0.2, random_state=42)#variar a percentagem split
+
+clf = OneVsRestClassifier(SVC())
+clf.fit(X_train, y_train)
+
+# Convert one-hot encoded labels to integer form
+y_pred = clf.predict(X_test)
+
+y_test_int = np.argmax(y_test, axis=1)
+y_pred_int = np.argmax(y_pred, axis=1)
+
+# Print classification report and confusion matrix
+
+mcc = matthews_corrcoef(y_test_int, y_pred_int)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+print(classification_report(y_test_int, y_pred_int))
+print(confusion_matrix(y_test_int, y_pred_int))
+
+#### AUtoencoder
+input = layers.Input(shape=(200, 200, 3))
+
+#valor alto apanha artefactos, e valor baixo nao captura bem as features da imagem
+latent_dim = 2000
+
+
+## Classe 
+
+# Encoder
+
+#didnt increase filter count in encoder and decrease equally in decoder because
+#it can lead to overfitting
+
+
+
+x = layers.Conv2D(64, (3,3), activation='relu', padding='same', strides = 2)(input)
+#x = layers.MaxPooling2D((2,2), padding='same')(x)#dividir imagem por factor de 2
+x = layers.Conv2D(32, (3,3), activation='relu', padding='same', strides = 2)(x)#reduziu se nr de filtros para 32 porque foi decidido factor de 32 filtros
+#x = layers.MaxPooling2D((2,2), padding='same')(x)
+x = layers.Conv2D(16, (3,3), activation='relu', padding='same', strides = 2)(x)
+#x = layers.MaxPooling2D((2,2), padding='same')(x)#25*25*16=10000
+x = layers.Flatten()(x)
+enc = layers.Dense(latent_dim, activation='relu')(x)
+
+# Decoder
+
+#having conv2D here helps to avoid checkerboard artifacts
+#which are alternating points of colors (like checkerboard)
+#that are consequence of deconvolutioning an image and cause 
+#loss of quality in final reconstruction
+#x = layers.Conv2D(16, (3,3), activation='relu', padding='same')(x)
+
+#equivalente a fazer dense layer no fim do encoder e no inicio do decoder
+#fazer a layer dense fica um vector
+#fazer reshape depois do 2 dense para voltar a ter imagem do vetor criado
+
+x = layers.Dense(25*25*16, activation='relu')(enc)
+x = layers.Reshape((25,25,16))(x)
+#x = layers.UpSampling2D((2,2))(x)
+x = layers.Conv2DTranspose(32, (3, 3), activation="relu", padding="same", strides = 2)(x)
+#x = layers.UpSampling2D((2,2))(x)
+x = layers.Conv2DTranspose(64, (3,3), activation='relu', padding='same', strides = 2)(x)
+#x = layers.UpSampling2D((2,2))(x)
+output = layers.Conv2DTranspose(3, (3,3), activation='sigmoid', padding='same', strides = 2)(x)
+
+
+#maybe use strides in the layers too
+#example:
+#x = layers.Conv2D(16, (3,3), activation='relu', padding='same', strides = 2)(x)
+
+
+# Autoencoder
+autoencoder = Model(input, output)
+
+# binary cross entropy is good for image feature extraction especially when images are normalized
+autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
+#autoencoder.summary()
+
+##### Gender
+X_train, X_test, y_train, y_test = train_test_split(images, genders, test_size=0.2, random_state=42)
+
+X_train = np.array(X_train)
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], X_train.shape[2], 3))
+
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], X_test.shape[2], 3))
+
+autoencoder.compile(optimizer="adam", loss="binary_crossentropy")
+autoencoder.summary()### Save this completly
+
+#using validation_data is good for detecting overfitting as 
+#it does early stopping when the performance is no longer improving
+
+autoencoder.fit(x = X_train, 
+                y = X_train,
+                epochs = 2,
+                shuffle = True,
+                batch_size = 32,
+                validation_data = (X_test, X_test),)
+
+new_model = Model(input, enc)
+
+pred_train = new_model.predict(X_train)
+pred_test = new_model.predict(X_test)
+
+encoded_X_train = pred_train.reshape((pred_train.shape[0], -1))
+encoded_X_test = pred_test.reshape((pred_test.shape[0], -1))
+
+clf = SVC()
+clf.fit(pred_train, y_train)
+
+# Evaluate the classifier on the testing data
+y_pred = clf.predict(pred_test)
+
+mcc = matthews_corrcoef(y_test, y_pred)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+#print("Accuracy: ", accuracy)
+print(classification_report(y_test, y_pred))
+#confusion_matrix(y_test, y_pred)
+
+labelGender = list(set(genders))
+
+# assume y_true and y_pred are the true and predicted labels, respectively
+cm = confusion_matrix(y_test, y_pred)
+
+# create a heatmap of the confusion matrix using seaborn
+sns.heatmap(cm, annot=True, cmap="Blues", fmt="d", xticklabels=labelGender, yticklabels=labelGender)
+
+# set plot labels and title
+plt.xlabel('Predicted Label')
+plt.ylabel('True Label')
+plt.title('Confusion Matrix')
+plt.savefig('CM SVM Gender AE.png')
+
+
+#### AGE
+
+X_train, X_test, y_train, y_test = train_test_split(images, age_categories, test_size=0.2, random_state=42)
+
+X_train = np.array(X_train)
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], X_train.shape[2], 3))
+
+X_test = np.array(X_test)
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], X_test.shape[2], 3))
+
+input = layers.Input(shape=(200, 200, 3))
+
+# binary cross entropy is good for image feature extraction especially when images are normalized
+autoencoder.compile(optimizer="adam", loss='categorical_crossentropy')
+autoencoder.summary()
+
+autoencoder.fit(x = X_train, 
+                y = X_train,
+                epochs = 2,
+                shuffle = True,
+                batch_size = 32,
+                validation_data = (X_test, X_test),
+)
+
+encoded_X_train = autoencoder.predict(X_train)
+encoded_X_test = autoencoder.predict(X_test)
+
+encoded_X_train = encoded_X_train.reshape((encoded_X_train.shape[0], -1))
+encoded_X_test = encoded_X_test.reshape((encoded_X_test.shape[0], -1))
+
+clf = OneVsRestClassifier(SVC())
+clf.fit(encoded_X_train, y_train)
+
+# Convert one-hot encoded labels to integer form
+y_pred = clf.predict(encoded_X_test)
+
+y_test_int = np.argmax(y_test, axis=1)
+y_pred_int = np.argmax(y_pred, axis=1)
+
+# Print classification report and confusion matrix
+
+mcc = matthews_corrcoef(y_test_int, y_pred_int)
+#accuracy = accuracy_score(y_test, y_pred)
+print("MCC: ", mcc)
+print(classification_report(y_test_int, y_pred_int))
+print(confusion_matrix(y_test_int, y_pred_int))
+
+
+
+finish_time = datetime.now()
+finish_time = finish_time.strftime("%H:%M:%S")
+
+print("Initial Time =", initial_time)
+print("Current Time =", finish_time)
